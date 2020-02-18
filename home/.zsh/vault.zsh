7b82e3db44dfa10416ab38154031446a26de2ecc
David Hohengaßner <dhohengassner@bethel.jw.org>
2020-02-18 21:42:48 +0100

    add travel_prd

--- home/.zsh/vault.zsh
+++ home/.zsh/vault.zsh
@@ -35,7 +35,7 @@ function vt() {
 		fi
 	}
 
-	awsaccounts=(lab1 lab2 hyperloop prd prd_read appsjw brdcst prd_leg)
+	awsaccounts=(lab1 lab2 hyperloop prd prd_read appsjw brdcst prd_leg travel_prd)
 	bethel_values=$(<~/bethel_values.json)
 
 	PS3='Select an account: '
@@ -71,6 +71,10 @@ function vt() {
 			;;
 		prd_leg)
 		  vaulty_authy "$(echo $bethel_values | jq -r '.vault.environment_creds.prd_leg')" "$1" $bethel_values
+			break
+			;;
+		travel_prd)
+		  vaulty_authy "$(echo $bethel_values | jq -r '.vault.environment_creds.travel_prd')" "$1" $bethel_values
 			break
 			;;
 		*)
